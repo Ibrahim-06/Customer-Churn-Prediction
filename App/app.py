907dd5ad42da65a7373ee55078ced432712d9567
Ibrahim Mohamed <Himakishk55@gmail.com>
2025-07-31 00:48:16 +0300

    Update app.py

--- App/app.py
+++ App/app.py
@@ -1,7 +1,15 @@
 import streamlit as st
 import pandas as pd
 import numpy as np
-import joblib
+import subprocess
+import sys
+
+try:
+    import joblib
+except ModuleNotFoundError:
+    subprocess.check_call([sys.executable, "-m", "pip", "install", "joblib"])
+    import joblib
+
 
 model = joblib.load("../Model/Logistic_model.pkl")
 scaler = joblib.load("../Scaler/Scaler.pkl")
